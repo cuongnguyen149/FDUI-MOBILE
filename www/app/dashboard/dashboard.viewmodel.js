29498e61ff667ac018beb916c6c06c5a03db7153
cuongnguyen <cuongnguyen149@gmail.com>
2016-07-19 02:02:49 +0700

    popup and popover

--- www/app/dashboard/dashboard.viewmodel.js
+++ www/app/dashboard/dashboard.viewmodel.js
@@ -5,11 +5,13 @@
 'use strict';
 
 (function (ViewModelBase) {
-    function DashboardViewModel($filter, searchService, todoService, $ionicSlideBoxDelegate) {
+    function DashboardViewModel($filter, searchService, todoService, $ionicSlideBoxDelegate, $ionicPopover, $ionicPopup) {
         this.$filter                    = $filter;
         this.$ionicSlideBoxDelegate     = $ionicSlideBoxDelegate;
+        this.$ionicPopover              = $ionicPopover;
         this.$searchService             = searchService;
         this.$todoService               = todoService;
+        this.$ionicPopup                = $ionicPopup;
         this.recentSearchCurrentPage    = fdui.const.pagination.currentPage;
         this.todoCurrentPage            = fdui.const.pagination.currentPage;
         this.pageSize                   = fdui.const.pagination.pageSize;
@@ -17,7 +19,7 @@
         ViewModelBase.call(this);
     }
 
-    DashboardViewModel.$inject = ['$filter', 'searchService', 'todoService', '$ionicSlideBoxDelegate'];
+    DashboardViewModel.$inject = ['$filter', 'searchService', 'todoService', '$ionicSlideBoxDelegate', '$ionicPopover', '$ionicPopup'];
 
     DashboardViewModel.prototype = Object.create(ViewModelBase.prototype);
     DashboardViewModel.prototype.constructor = DashboardViewModel;
@@ -27,19 +29,30 @@
         console.log('init dashboardViewModel');
         var userName = self.getLocalStorageItem("userName");
 
-        this.$searchService.getSimpleSearchResultAsync().then(function(searchRecentResponse) {
-        //     self.searchRecentResponse = searchRecentResponse.responseEntity.filter(function(obj) {
-        //         return obj.type !== 'ADVANCED_SEARCH';
-        //     });
-        //     self.arrayPageNumber = self.getArrayPageNumber(self.searchRecentResponse.length);    
-            self.searchRecentResponse = [{name: 'tesst'},{name: 'tesst'}];
-            self.arrayPageNumber = [0, 1];
-            self.$ionicSlideBoxDelegate.update();
-            self.isSearchRecentLoading = false; 
-        });
+        // this.$searchService.getSimpleSearchResultAsync().then(function(searchRecentResponse) {
+        // //     self.searchRecentResponse = searchRecentResponse.responseEntity.filter(function(obj) {
+        // //         return obj.type !== 'ADVANCED_SEARCH';
+        // //     });
+        // //     self.arrayPageNumber = self.getArrayPageNumber(self.searchRecentResponse.length);    
+        //     self.searchRecentResponse = [{name: 'tesst'},{name: 'tesst'}];
+        //     self.arrayPageNumber = [0, 1];
+        //     self.$ionicSlideBoxDelegate.update();
+        //     self.isSearchRecentLoading = false; 
+        // });
         // this.$todoService.getToDoTileAsync().then(function(todoResponse) {
         //     self.todoResponse = todoResponse.responseEntity.resultsetdata;
         // });
+        self.$ionicPopover.fromTemplateUrl('./popupandpopover/popoveruser.html', {
+           self: this 
+        }).then(function(popoverUser) {
+           self.popoverUser = popoverUser;
+        });
+
+        self.$ionicPopover.fromTemplateUrl('./popupandpopover/popoverMore.html', {
+           self: this
+        }).then(function(popoverMore) {
+           self.popoverMore = popoverMore;
+        });
     };
 
     DashboardViewModel.prototype.dispose = function () {
@@ -56,6 +69,21 @@
 
     DashboardViewModel.prototype.docClick = function () {
       console.log('Doc click');
+    };
+    DashboardViewModel.prototype.logoutClick = function () {
+        this.$ionicPopup.show({
+            title: 'Logout Confirmation',
+            templateUrl: './popupandpopover/popuplogout.html',
+            buttons: [{
+                text: '<b>Yes</b>',
+                type: '',
+                onTap: function(e) {
+                //do stuff
+                }
+            }, {
+                text: 'No'
+            }]
+        });
     };
 
     fdui.viewModel('dashboardViewModel', DashboardViewModel);
